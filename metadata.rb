6b7ae51e1ed3314d8695feba028a7b9951781938
qubitrenagade <qubitrenegade@gmail.com>
2019-08-11 23:59:06 -0600

    update metdata

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,15 @@
-name 'certbot-exec-cloudflare'
-maintainer 'Qubit Renegade'
-maintainer_email 'qubitrenegade@protonmail.com'
-license 'MIT'
-description 'Installs/Configures certbot-exec-cloudflare'
-long_description File.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version '0.1.0'
-chef_version '>= 13.0'
+name              'certbot-exec-cloudflare'
+maintainer        'Qubit Renegade'
+maintainer_email  'qubitrenegade@protonmail.com'
+license           'MIT'
+description       'Installs/Configures certbot-exec-cloudflare'
+long_description  File.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           '0.1.0'
+chef_version      '>= 13.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/certbot-exec-cloudflare/issues'
+issues_url        'https://github.com/qubitrenegade/certbot-exec-cloudflare/issues'
+source_url        'https://github.com/qubitrenegade/certbot-exec-cloudflare'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/certbot-exec-cloudflare'
+%w(ubuntu centos).each do |platform|
+  supports platform
+end
