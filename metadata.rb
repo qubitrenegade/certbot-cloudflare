f003e1ce4460dc181ca4c1c6ae7c0e577f170d0f
qubitrenagade <qubitrenegade@gmail.com>
2019-08-12 00:24:23 -0600

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Qubit Renegade'
 maintainer_email  'qubitrenegade@protonmail.com'
 license           'MIT'
 description       'Installs/Configures certbot-exec-cloudflare'
-long_description  File.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description  File.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
 chef_version      '>= 13.0'
 
